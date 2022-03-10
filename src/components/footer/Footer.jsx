6791dac4c2f59793e359d4f350ccc2923b83b682
Tristan <frostedtv.pro@gmail.com>
2022-03-09 22:10:31 -0500

    updated logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className='container'>
         <div className='footer-div'>
-            <h3>Droits réservé © ProLookAuto</h3>
+            <h3>Droits réservé © Pro Look Auto</h3>
             <h3>Site web développé avec <span>❤</span> par Tristan Landry</h3>
         </div>
     </div>
